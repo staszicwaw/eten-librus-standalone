cfac013789cadd2b5f2b8c259f7ab1ef04c7c412
MasloMaslane <matimasiarz@gmail.com>
2023-06-28 16:12:30 +0200

    Remove webhook link from git (xd)

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ fi
 
 /home/pi/.nvm/versions/node/v20.2.0/bin/node dist
 touch data/crashed
-curl -X POST "https://discord.com/api/webhooks/1033019393644429352/4_VjEkWs97c4pqXVFy6U_-cClI4DK9Bac-RIzNUZpVSr5Jv3dG3GjVDaVHT9ciFlqSav" -H "Content-Type: application/json" --data-binary @- <<DATA
+curl -X POST `cat webhook-link` -H "Content-Type: application/json" --data-binary @- <<DATA
 {
 "content": "<@895752707032367145> It's dead, jim."
 }
